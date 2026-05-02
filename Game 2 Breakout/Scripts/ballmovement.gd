6b3ffc72756646063794300613c8d44abef98ff0
loyab1 <loyab1@montclair.edu>
2026-05-02 13:14:27 -0400

    [Breakout] Updated Ball Script and Scene along with Readme Current goal

--- Game 2 Breakout/Scripts/ballmovement.gd
+++ Game 2 Breakout/Scripts/ballmovement.gd
@@ -4,8 +4,12 @@ extends CharacterBody2D
 var speed : int = 200
 var velocity_vector : Vector2  = Vector2(0,-1)
 
+
+func _ready() -> void:
+	global_position = get_viewport_rect().size / 2
+
 func _physics_process(delta: float) -> void:
-	var collision_info = move_and_collide(velocity_vector * speed * delta)
+	var collision_info : KinematicCollision2D = move_and_collide(velocity_vector * speed * delta)
 	
 	if collision_info:
 		velocity_vector = velocity_vector.bounce(collision_info.get_normal())
